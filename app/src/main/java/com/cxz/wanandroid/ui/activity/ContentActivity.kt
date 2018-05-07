e8bd08cf810836c87b642efc761ce0277051b653
chenxz <chenxiaozhen@ieforever.com>
2018-05-07 23:19:20 +0800

    commit

---
+++ app/src/main/java/com/cxz/wanandroid/ui/activity/ContentActivity.kt
@@ -0,0 +1,150 @@
+package com.cxz.wanandroid.ui.activity
+
+import android.content.Intent
+import android.net.Uri
+import android.view.KeyEvent
+import android.view.Menu
+import android.view.MenuItem
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.LinearLayout
+import com.cxz.wanandroid.R
+import com.cxz.wanandroid.base.BaseActivity
+import com.cxz.wanandroid.common.Contanst
+import com.cxz.wanandroid.ext.getAgentWeb
+import com.just.agentweb.AgentWeb
+import com.just.agentweb.ChromeClientCallbackManager
+import kotlinx.android.synthetic.main.container.*
+import kotlinx.android.synthetic.main.toolbar.*
+
+class ContentActivity : BaseActivity() {
+
+    private lateinit var agentWeb: AgentWeb
+    private lateinit var shareTitle: String
+    private lateinit var shareUrl: String
+    private var shareId: Int = 0
+
+    override fun attachLayoutRes(): Int = R.layout.activity_content
+
+    override fun initData() {
+    }
+
+    override fun initView() {
+        toolbar.run {
+            title = getString(R.string.loading)
+            setSupportActionBar(this)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        intent.extras.let {
+            shareId = it.getInt(Contanst.CONTENT_ID_KEY)
+            shareTitle = it.getString(Contanst.CONTENT_TITLE_KEY)
+            shareUrl = it.getString(Contanst.CONTENT_URL_KEY)
+        }
+        agentWeb = shareUrl.getAgentWeb(this, container,
+                LinearLayout.LayoutParams(-1, -1),
+                receivedTitleCallback,
+                webChromeClient, webViewClient)
+
+    }
+
+    override fun start() {
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_content, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            R.id.action_share -> {
+                Intent().run {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT,
+                            getString(
+                                    R.string.share_article_url,
+                                    getString(R.string.app_name),
+                                    shareTitle,
+                                    shareUrl
+                            ))
+                    type = Contanst.CONTENT_SHARE_TYPE
+                    startActivity(Intent.createChooser(this, getString(R.string.action_share)))
+                }
+                return true
+            }
+            R.id.action_like -> {
+
+                return true
+            }
+            R.id.action_browser -> {
+                Intent().run {
+                    action = "android.intent.action.VIEW"
+                    data = Uri.parse(shareUrl)
+                    startActivity(this)
+                }
+                return true
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+
+        return if (agentWeb?.handleKeyEvent(keyCode, event)!!) {
+            true
+        } else {
+            finish()
+            super.onKeyDown(keyCode, event)
+        }
+    }
+
+    override fun onResume() {
+        agentWeb.webLifeCycle.onResume()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        agentWeb.webLifeCycle.onPause()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        agentWeb.webLifeCycle.onDestroy()
+        super.onDestroy()
+    }
+
+    /**
+     * receivedTitleCallback
+     */
+    private val receivedTitleCallback =
+            ChromeClientCallbackManager.ReceivedTitleCallback { _, title ->
+                title?.let {
+                    toolbar.title = it
+                }
+            }
+
+    /**
+     * webViewClient
+     */
+    private val webViewClient = object : WebViewClient() {
+    }
+
+    /**
+     * webChromeClient
+     */
+    private val webChromeClient = object : WebChromeClient() {
+        override fun onProgressChanged(view: WebView, newProgress: Int) {
+        }
+
+        override fun onReceivedTitle(view: WebView, title: String) {
+            super.onReceivedTitle(view, title)
+        }
+    }
+
+}
--- app/src/main/java/com/cxz/wanandroid/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/cxz/wanandroid/ui/fragment/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.cxz.wanandroid.ui.fragment
 
+import android.content.Intent
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
@@ -11,12 +12,13 @@ import com.chad.library.adapter.base.BaseQuickAdapter
 import com.cxz.wanandroid.R
 import com.cxz.wanandroid.adapter.HomeAdapter
 import com.cxz.wanandroid.base.BaseFragment
-import com.cxz.wanandroid.ext.loge
+import com.cxz.wanandroid.common.Contanst
 import com.cxz.wanandroid.mvp.contract.HomeContract
 import com.cxz.wanandroid.mvp.model.bean.Article
 import com.cxz.wanandroid.mvp.model.bean.ArticleResponseBody
 import com.cxz.wanandroid.mvp.model.bean.Banner
 import com.cxz.wanandroid.mvp.presenter.HomePresenter
+import com.cxz.wanandroid.ui.activity.ContentActivity
 import io.reactivex.Observable
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.item_home_banner.view.*
@@ -36,12 +38,23 @@ class HomeFragment : BaseFragment(), HomeContract.View {
 
     private val datas = mutableListOf<Article>()
 
+    private lateinit var bannerDatas: ArrayList<Banner>
+
     private val bannerView by lazy {
         layoutInflater.inflate(R.layout.item_home_banner, null)
     }
 
     private val homeAdapter: HomeAdapter by lazy {
         HomeAdapter(activity, datas)
+    }
+
+    private val bannerAdapter: BGABanner.Adapter<ImageView, String> by lazy {
+        BGABanner.Adapter<ImageView, String> { bgaBanner, imageView, feedImageUrl, position ->
+            Glide.with(activity)
+                    .load(feedImageUrl)
+                    .transition(DrawableTransitionOptions().crossFade())
+                    .into(imageView)
+        }
     }
 
     override fun attachLayoutRes(): Int = R.layout.fragment_home
@@ -69,6 +82,10 @@ class HomeFragment : BaseFragment(), HomeContract.View {
             addHeaderView(bannerView)
         }
 
+        bannerView.banner.run {
+            setDelegate(bannerDelegate)
+        }
+
     }
 
     override fun lazyLoad() {
@@ -88,6 +105,7 @@ class HomeFragment : BaseFragment(), HomeContract.View {
     }
 
     override fun setBanner(banners: List<Banner>) {
+        bannerDatas = banners as ArrayList<Banner>
         val bannerFeedList = ArrayList<String>()
         val bannerTitleList = ArrayList<String>()
         Observable.fromIterable(banners)
@@ -98,16 +116,8 @@ class HomeFragment : BaseFragment(), HomeContract.View {
         bannerView.banner.run {
             setAutoPlayAble(bannerFeedList.size > 1)
             setData(bannerFeedList, bannerTitleList)
-            setAdapter(object : BGABanner.Adapter<ImageView, String> {
-                override fun fillBannerItem(bgaBanner: BGABanner?, imageView: ImageView?, feedImageUrl: String?, position: Int) {
-                    Glide.with(activity)
-                            .load(feedImageUrl)
-                            .transition(DrawableTransitionOptions().crossFade())
-                            .into(imageView)
-                }
-            })
+            setAdapter(bannerAdapter)
         }
-
     }
 
     override fun setArticles(articles: ArticleResponseBody) {
@@ -147,6 +157,27 @@ class HomeFragment : BaseFragment(), HomeContract.View {
     private val onItemClickListener = BaseQuickAdapter.OnItemClickListener { _, _, position ->
         if (datas.size != 0) {
             val data = datas[position]
+            Intent(activity, ContentActivity::class.java).run {
+                putExtra(Contanst.CONTENT_URL_KEY, data.link)
+                putExtra(Contanst.CONTENT_TITLE_KEY, data.title)
+                putExtra(Contanst.CONTENT_ID_KEY, data.id)
+                startActivity(this)
+            }
+        }
+    }
+
+    /**
+     * BannerClickListener
+     */
+    private val bannerDelegate = BGABanner.Delegate<ImageView, String> { banner, imageView, model, position ->
+        if (bannerDatas.size > 0) {
+            val data = bannerDatas[position]
+            Intent(activity, ContentActivity::class.java).run {
+                putExtra(Contanst.CONTENT_URL_KEY, data.url)
+                putExtra(Contanst.CONTENT_TITLE_KEY, data.title)
+                putExtra(Contanst.CONTENT_ID_KEY, data.id)
+                startActivity(this)
+            }
         }
     }
 
