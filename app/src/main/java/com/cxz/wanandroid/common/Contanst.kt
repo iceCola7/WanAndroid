6b97d959376ea4ac546c80929570d72653fb1600
chenxz <chenxiaozhen@ieforever.com>
2018-05-07 23:21:14 +0800

    commit

--- app/src/main/java/com/cxz/wanandroid/common/Contanst.kt
+++ app/src/main/java/com/cxz/wanandroid/common/Contanst.kt
@@ -7,6 +7,23 @@ package com.cxz.wanandroid.common
 object Contanst{
 
     const val BASE_URL = "http://wanandroid.com/"
-    
+
+    /**
+     * url key
+     */
+    const val CONTENT_URL_KEY = "url"
+    /**
+     * title key
+     */
+    const val CONTENT_TITLE_KEY = "title"
+    /**
+     * id key
+     */
+    const val CONTENT_ID_KEY = "id"
+
+    /**
+     * share key
+     */
+    const val CONTENT_SHARE_TYPE = "text/plain"
 
 }
