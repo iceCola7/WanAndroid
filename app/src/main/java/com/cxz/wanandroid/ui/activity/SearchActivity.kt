0421376d1a83dbe2e2c86bb20f8b89fcb45848cb
chenxz <chenxiaozhen@ieforever.com>
2018-07-07 22:12:51 +0800

    update

--- app/src/main/java/com/cxz/wanandroid/ui/activity/SearchActivity.kt
+++ app/src/main/java/com/cxz/wanandroid/ui/activity/SearchActivity.kt
@@ -1,10 +1,10 @@
 package com.cxz.wanandroid.ui.activity
 
+import com.cxz.wanandroid.R
 import com.cxz.wanandroid.base.BaseActivity
 import com.cxz.wanandroid.mvp.contract.SearchContract
 import com.cxz.wanandroid.mvp.presenter.SearchPresenter
-import kotlinx.android.synthetic.main.toolbar.*
-import com.cxz.wanandroid.R
+import kotlinx.android.synthetic.main.toolbar_search.*
 
 class SearchActivity : BaseActivity(), SearchContract.View {
 
@@ -23,7 +23,7 @@ class SearchActivity : BaseActivity(), SearchContract.View {
     override fun initView() {
         mPresenter.attachView(this)
 
-        toolbar.run {
+        search_toolbar.run {
             title = ""
             setSupportActionBar(this)
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
