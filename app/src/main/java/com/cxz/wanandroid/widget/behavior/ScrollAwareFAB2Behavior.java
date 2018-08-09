64806ac36b7ddcdfb210170c34c1c02e564e3046
chenxz <563859095@qq.com>
2018-08-09 21:31:27 +0800

    mod behavior

--- app/src/main/java/com/cxz/wanandroid/widget/behavior/ScrollAwareFAB2Behavior.java
+++ app/src/main/java/com/cxz/wanandroid/widget/behavior/ScrollAwareFAB2Behavior.java
@@ -1,10 +1,9 @@
 package com.cxz.wanandroid.widget.behavior;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewCompat;
-import android.support.v4.view.ViewPropertyAnimatorListener;
-import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -12,67 +11,39 @@ import com.github.clans.fab.FloatingActionMenu;
 
 public class ScrollAwareFAB2Behavior extends CoordinatorLayout.Behavior<FloatingActionMenu> {
 
-    private static final android.view.animation.Interpolator INTERPOLATOR = new FastOutSlowInInterpolator();
-    private boolean mIsAnimatingOut = false;
+    private ObjectAnimator outAnimator, inAnimator;
 
     public ScrollAwareFAB2Behavior(Context context, AttributeSet attrs) {
-        super();
+        super(context, attrs);
     }
 
+    // 垂直滑动
     @Override
     public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionMenu child, View directTargetChild, View target, int nestedScrollAxes) {
-        //处理垂直方向上的滚动事件
-        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
     }
 
     @Override
-    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionMenu child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
-        //向上滚动进入，向下滚动隐藏
-        if (dyConsumed > 0 && !this.mIsAnimatingOut && child.getVisibility() == View.VISIBLE) {
-            //如果是展开的话就先收回去
+    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, FloatingActionMenu child, View target, int dx, int dy, int[] consumed) {
+        if (dy > 0) {// 上滑隐藏
             if (child.isOpened()) {
                 child.close(true);
-            } else {
-                child.open(true);
             }
-            //animateOut()和animateIn()都是私有方法，需要重新实现
-            animateOut(child);
-        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
-            animateIn(child);
+            if (outAnimator == null) {
+                outAnimator = ObjectAnimator.ofFloat(child, "translationY", 0, child.getHeight());
+                outAnimator.setDuration(200);
+            }
+            if (!outAnimator.isRunning() && child.getTranslationY() <= 0) {
+                outAnimator.start();
+            }
+        } else if (dy < 0) {// 下滑显示
+            if (inAnimator == null) {
+                inAnimator = ObjectAnimator.ofFloat(child, "translationY", child.getHeight(), 0);
+                inAnimator.setDuration(200);
+            }
+            if (!inAnimator.isRunning() && child.getTranslationY() >= child.getHeight()) {
+                inAnimator.start();
+            }
         }
-    }
-
-    private void animateOut(final FloatingActionMenu button) {
-        CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) button.getLayoutParams();
-        int fabBottomMargin = lp.bottomMargin;
-        int distanceToScroll = button.getHeight() + fabBottomMargin;
-        ViewCompat.animate(button).translationY(distanceToScroll)
-                .setInterpolator(INTERPOLATOR).withLayer()
-                .setListener(new ViewPropertyAnimatorListener() {
-                    @Override
-                    public void onAnimationStart(View view) {
-                        ScrollAwareFAB2Behavior.this.mIsAnimatingOut = true;
-                    }
-
-                    @Override
-                    public void onAnimationEnd(View view) {
-                        ScrollAwareFAB2Behavior.this.mIsAnimatingOut = false;
-                        view.setVisibility(View.GONE);
-                    }
-
-                    @Override
-                    public void onAnimationCancel(View view) {
-                        ScrollAwareFAB2Behavior.this.mIsAnimatingOut = false;
-
-                    }
-                }).start();
-    }
-
-    private void animateIn(FloatingActionMenu button) {
-        button.setVisibility(View.VISIBLE);
-        ViewCompat.animate(button).translationY(0)
-                .setInterpolator(INTERPOLATOR).withLayer().setListener(null)
-                .start();
     }
 }
