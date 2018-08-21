543bef1dc34878910be339c9256ac49782f397b6
chenxz <563859095@qq.com>
2018-08-22 00:03:31 +0800

    增加请求重连

---
+++ app/src/main/java/com/cxz/wanandroid/http/function/RetryWithDelay.kt
@@ -0,0 +1,52 @@
+package com.cxz.wanandroid.http.function
+
+import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
+import io.reactivex.functions.Function
+import retrofit2.HttpException
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+/**
+ * @author chenxz
+ * @date 2018/8/21
+ * @desc 请求重连
+ */
+class RetryWithDelay : Function<Observable<out Throwable>, Observable<*>> {
+
+    private var maxRetryCount = 3 // 可重试次数
+    private var retryDelayMillis: Long = 5000 // 重试等待时间
+
+    constructor() {}
+
+    constructor(retryDelayMillis: Long) {
+        this.retryDelayMillis = retryDelayMillis
+    }
+
+    constructor(maxRetryCount: Int, retryDelayMillis: Long) {
+        this.maxRetryCount = maxRetryCount
+        this.retryDelayMillis = retryDelayMillis
+    }
+
+    @Throws(Exception::class)
+    override fun apply(observable: Observable<out Throwable>): Observable<*> {
+        return observable
+                .zipWith(Observable.range(1, maxRetryCount + 1),
+                        BiFunction<Throwable, Int, Wrapper> { t1, t2 -> Wrapper(t2, t1) })
+                .flatMap { wrapper ->
+                    val t = wrapper.throwable
+                    if ((t is ConnectException
+                                    || t is SocketTimeoutException
+                                    || t is TimeoutException
+                                    || t is HttpException)
+                            && wrapper.index < maxRetryCount + 1) {
+                        Observable.timer(retryDelayMillis * wrapper.index, TimeUnit.SECONDS)
+                    } else Observable.error<Any>(wrapper.throwable)
+                }
+    }
+
+    private inner class Wrapper(val index: Int, val throwable: Throwable)
+
+}
