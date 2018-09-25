1194aab9a6d58f6c92debd5df551e5bed46979ee
chenxz <563859095@qq.com>
2018-09-25 09:52:11 +0800

    mod

--- app/src/main/java/com/cxz/wanandroid/adapter/KnowledgeTreeAdapter.kt
+++ app/src/main/java/com/cxz/wanandroid/adapter/KnowledgeTreeAdapter.kt
@@ -1,6 +1,7 @@
 package com.cxz.wanandroid.adapter
 
 import android.content.Context
+import android.text.Html
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.cxz.wanandroid.R
@@ -16,7 +17,7 @@ class KnowledgeTreeAdapter(private val context: Context?, datas: MutableList<Kno
         item?.children.let {
             helper?.setText(R.id.title_second,
                     it?.joinToString("    ", transform = { child ->
-                        child.name
+                        Html.fromHtml(child.name)
                     })
             )
 
