ec8cd0ec2666bcea80fffb410ee89905f769ed23
chenxz <563859095@qq.com>
2018-09-26 11:04:38 +0800

    add HeaderInterceptor

---
+++ app/src/main/java/com/cxz/wanandroid/http/interceptor/HeaderInterceptor.kt
@@ -0,0 +1,46 @@
+package com.cxz.wanandroid.http.interceptor
+
+import com.cxz.wanandroid.constant.HttpConstant
+import com.cxz.wanandroid.utils.Preference
+import okhttp3.Interceptor
+import okhttp3.Response
+
+/**
+ * @author chenxz
+ * @date 2018/9/26
+ * @desc HeaderInterceptor: 设置请求头
+ */
+class HeaderInterceptor : Interceptor {
+
+    /**
+     * token
+     */
+    private var token: String by Preference("token", "")
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+
+        val request = chain.request()
+        val builder = request.newBuilder()
+
+        builder.addHeader("Content-type", "application/json; charset=utf-8")
+                // .header("token", token)
+                // .method(request.method(), request.body())
+
+        val domain = request.url().host()
+        val url = request.url().toString()
+        if (domain.isNotEmpty() && (url.contains(HttpConstant.COLLECTIONS_WEBSITE)
+                        || url.contains(HttpConstant.UNCOLLECTIONS_WEBSITE)
+                        || url.contains(HttpConstant.ARTICLE_WEBSITE)
+                        || url.contains(HttpConstant.TODO_WEBSITE))) {
+            val spDomain: String by Preference(domain, "")
+            val cookie: String = if (spDomain.isNotEmpty()) spDomain else ""
+            if (cookie.isNotEmpty()) {
+                // 将 Cookie 添加到请求头
+                builder.addHeader(HttpConstant.COOKIE_NAME, cookie)
+            }
+        }
+
+        return chain.proceed(builder.build())
+    }
+
+}
