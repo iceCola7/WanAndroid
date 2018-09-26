e5607288128bb4bd283743ee551d89ef155ee5ea
chenxz <563859095@qq.com>
2018-09-26 11:04:11 +0800

    add CacheInterceptor

---
+++ app/src/main/java/com/cxz/wanandroid/http/interceptor/CacheInterceptor.kt
@@ -0,0 +1,41 @@
+package com.cxz.wanandroid.http.interceptor
+
+import com.cxz.wanandroid.app.App
+import com.cxz.wanandroid.utils.NetWorkUtil
+import okhttp3.CacheControl
+import okhttp3.Interceptor
+import okhttp3.Response
+
+/**
+ * @author chenxz
+ * @date 2018/9/26
+ * @desc CacheInterceptor: 设置缓存
+ */
+class CacheInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var request = chain.request()
+        if (!NetWorkUtil.isNetworkAvailable(App.context)) {
+            request = request.newBuilder()
+                    .cacheControl(CacheControl.FORCE_CACHE)
+                    .build()
+        }
+        val response = chain.proceed(request)
+        if (NetWorkUtil.isNetworkAvailable(App.context)) {
+            val maxAge = 0
+            // 有网络时 设置缓存超时时间0个小时 ,意思就是不读取缓存数据,只对get有用,post没有缓冲
+            response.newBuilder()
+                    .header("Cache-Control", "public, max-age=$maxAge")
+                    .removeHeader("Retrofit")// 清除头信息，因为服务器如果不支持，会返回一些干扰信息，不清除下面无法生效
+                    .build()
+        } else {
+            // 无网络时，设置超时为4周  只对get有用,post没有缓冲
+            val maxStale = 60 * 60 * 24 * 28
+            response.newBuilder()
+                    .header("Cache-Control", "public, only-if-cached, max-stale=$maxStale")
+                    .removeHeader("nyn")
+                    .build()
+        }
+        return response
+    }
+}
