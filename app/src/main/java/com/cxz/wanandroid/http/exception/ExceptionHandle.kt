fec52269254477159df49a1a9e73670fb51794c0
chenxz <563859095@qq.com>
2018-11-06 23:02:59 +0800

    mod

--- app/src/main/java/com/cxz/wanandroid/http/exception/ExceptionHandle.kt
+++ app/src/main/java/com/cxz/wanandroid/http/exception/ExceptionHandle.kt
@@ -13,30 +13,31 @@ import java.text.ParseException
  */
 class ExceptionHandle {
     companion object {
+        private const val TAG = "ExceptionHandle"
         var errorCode = ErrorStatus.UNKNOWN_ERROR
         var errorMsg = "请求失败，请稍后重试"
 
         fun handleException(e: Throwable): String {
             e.printStackTrace()
             if (e is SocketTimeoutException) {//网络超时
-                Logger.e("TAG", "网络连接异常: " + e.message)
+                Logger.e(TAG, "网络连接异常: " + e.message)
                 errorMsg = "网络连接异常"
                 errorCode = ErrorStatus.NETWORK_ERROR
             } else if (e is ConnectException) { //均视为网络错误
-                Logger.e("TAG", "网络连接异常: " + e.message)
+                Logger.e(TAG, "网络连接异常: " + e.message)
                 errorMsg = "网络连接异常"
                 errorCode = ErrorStatus.NETWORK_ERROR
             } else if (e is JsonParseException
                     || e is JSONException
                     || e is ParseException) {   //均视为解析错误
-                Logger.e("TAG", "数据解析异常: " + e.message)
+                Logger.e(TAG, "数据解析异常: " + e.message)
                 errorMsg = "数据解析异常"
                 errorCode = ErrorStatus.SERVER_ERROR
             } else if (e is ApiException) {//服务器返回的错误信息
                 errorMsg = e.message.toString()
                 errorCode = ErrorStatus.SERVER_ERROR
             } else if (e is UnknownHostException) {
-                Logger.e("TAG", "网络连接异常: " + e.message)
+                Logger.e(TAG, "网络连接异常: " + e.message)
                 errorMsg = "网络连接异常"
                 errorCode = ErrorStatus.NETWORK_ERROR
             } else if (e is IllegalArgumentException) {
@@ -44,11 +45,10 @@ class ExceptionHandle {
                 errorCode = ErrorStatus.SERVER_ERROR
             } else {//未知错误
                 try {
-                    Logger.e("TAG", "错误: " + e.message)
+                    Logger.e(TAG, "错误: " + e.message)
                 } catch (e1: Exception) {
-                    Logger.e("TAG", "未知错误Debug调试 ")
+                    Logger.e(TAG, "未知错误Debug调试 ")
                 }
-
                 errorMsg = "未知错误，可能抛锚了吧~"
                 errorCode = ErrorStatus.UNKNOWN_ERROR
             }
