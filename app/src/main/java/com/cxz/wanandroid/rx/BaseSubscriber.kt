7450869ca5389d8201fb8697fc8a857bf7e31bc3
chenxz <563859095@qq.com>
2018-11-24 00:08:48 +0800

    mod base

--- app/src/main/java/com/cxz/wanandroid/rx/BaseSubscriber.kt
+++ app/src/main/java/com/cxz/wanandroid/rx/BaseSubscriber.kt
@@ -1,36 +1,48 @@
-package com.cxz.wanandroid.rx
+package com.cxz.kotlin.baselibs.rx
 
+import com.cxz.wanandroid.app.App
 import com.cxz.wanandroid.base.IView
+import com.cxz.wanandroid.http.exception.ErrorStatus
 import com.cxz.wanandroid.http.exception.ExceptionHandle
+import com.cxz.wanandroid.mvp.model.bean.BaseBean
+import com.cxz.wanandroid.utils.NetWorkUtil
 import io.reactivex.subscribers.ResourceSubscriber
 
 /**
  * Created by chenxz on 2018/6/11.
  */
-abstract class BaseSubscriber<T> : ResourceSubscriber<T> {
+abstract class BaseSubscriber<T : BaseBean>(view: IView? = null) : ResourceSubscriber<T>() {
 
-    private var mView: IView? = null
-    private var mErrorMsg: String = ""
+    private var mView = view
 
-    constructor(view: IView) {
-        this.mView = view
-    }
-
-    constructor(view: IView, errorMsg: String) {
-        this.mView = view
-        this.mErrorMsg = errorMsg
-    }
+    abstract fun onSuccess(t: T)
 
     override fun onComplete() {
+        mView?.hideLoading()
     }
 
-    override fun onError(e: Throwable) {
-        mView ?: return
-        if (mErrorMsg.isNotEmpty()) {
-            mView?.showError(mErrorMsg)
-        } else {
-            mErrorMsg = ExceptionHandle.handleException(e)
-            mView?.showError(mErrorMsg)
+    override fun onStart() {
+        super.onStart()
+        mView?.showLoading()
+        if (!NetWorkUtil.isNetworkConnected(App.instance)) {
+            mView?.showDefaultMsg("当前网络不可用，请检查网络设置")
+            onComplete()
         }
     }
+
+    override fun onNext(t: T) {
+        when {
+            t.errorCode == ErrorStatus.SUCCESS -> onSuccess(t)
+            t.errorCode == ErrorStatus.TOKEN_INVAILD -> {
+                // Token 过期，重新登录
+            }
+            else -> mView?.showDefaultMsg(t.errorMsg)
+        }
+    }
+
+    override fun onError(t: Throwable) {
+        mView?.hideLoading()
+        ExceptionHandle.handleException(t)
+    }
+
 }
