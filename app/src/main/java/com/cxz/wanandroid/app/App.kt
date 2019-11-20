12f72d28b55c8666d712864f031453583dfbf07f
chenxz <563859095@qq.com>
2019-11-20 16:46:55 +0800

    fix bugly config

--- app/src/main/java/com/cxz/wanandroid/app/App.kt
+++ app/src/main/java/com/cxz/wanandroid/app/App.kt
@@ -75,49 +75,66 @@ class App : Application() {
         if (BuildConfig.DEBUG) {
             return
         }
-        // 获取当前包名
-        val packageName = applicationContext.packageName
+        /**
+         * true表示app启动自动初始化升级模块; false不会自动初始化;
+         * 开发者如果担心sdk初始化影响app启动速度，可以设置为false，
+         * 在后面某个时刻手动调用Beta.init(getApplicationContext(),false);
+         */
+        //Beta.autoInit = false
+        /**
+         * true表示初始化时自动检查升级; false表示不会自动检查升级,需要手动调用Beta.checkUpgrade()方法;
+         */
+        Beta.autoCheckUpgrade = false
+        /**
+         * 设置升级检查周期为60s(默认检查周期为0s)，60s内SDK不重复向后台请求策略);
+         */
+        //Beta.upgradeCheckPeriod = 60 * 1000
+        /**
+         * 设置启动延时为1s（默认延时3s），APP启动1s后初始化SDK，避免影响APP启动速度;
+         */
+        Beta.initDelay = 1 * 1000
+        /**
+         * 设置通知栏大图标，largeIconId为项目中的图片资源;
+         */
+        Beta.largeIconId = R.mipmap.ic_launcher
+        /**
+         * 设置状态栏小图标，smallIconId为项目中的图片资源Id;
+         */
+        Beta.smallIconId = R.mipmap.ic_launcher
+        /**
+         * 设置更新弹窗默认展示的banner，defaultBannerId为项目中的图片资源Id;
+         * 当后台配置的banner拉取失败时显示此banner，默认不设置则展示“loading“;
+         */
+        Beta.defaultBannerId = R.mipmap.ic_launcher
+        /**
+         * 设置sd卡的Download为更新资源保存目录;
+         * 后续更新资源会保存在此目录，需要在manifest中添加WRITE_EXTERNAL_STORAGE权限;
+         */
+        Beta.storageDir =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        /**
+         * 已经确认过的弹窗在APP下次启动自动检查更新时会再次显示;
+         */
+        Beta.showInterruptedStrategy = false
+        /**
+         * 只允许在MainActivity上显示更新弹窗，其他activity上不显示弹窗; 不设置会默认所有activity都可以显示弹窗;
+         */
+        //Beta.canShowUpgradeActs.add(MainActivity::class.java)
+
         // 获取当前进程名
         val processName = CommonUtil.getProcessName(android.os.Process.myPid())
         // 设置是否为上报进程
         val strategy = CrashReport.UserStrategy(applicationContext)
-        strategy.isUploadProcess = false || processName == packageName
-        // 设置sd卡的Download为更新资源保存目录;
-        Beta.storageDir =
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        strategy.isUploadProcess = false || processName == applicationContext.packageName
 
-        Beta.upgradeStateListener = object : UpgradeStateListener {
-            override fun onDownloadCompleted(isManual: Boolean) {
-            }
-
-            override fun onUpgradeSuccess(isManual: Boolean) {
-            }
-
-            override fun onUpgradeFailed(isManual: Boolean) {
-                if (isManual) {
-                    showToast(getString(R.string.check_version_fail))
-                }
-            }
-
-            override fun onUpgrading(isManual: Boolean) {
-                if (isManual) {
-                    showToast(getString(R.string.check_version_ing))
-                }
-            }
-
-            override fun onUpgradeNoVersion(isManual: Boolean) {
-                if (isManual) {
-                    showToast(getString(R.string.check_no_version))
-                }
-            }
-        }
+        Beta.upgradeStateListener = upgradeStateListener
 
         // 自定义更新布局要设置在 init 之前
         // R.layout.layout_upgrade_dialog 文件要注意两点
         // 注意1: 半透明背景要自己加上
         // 注意2: 即使自定义的弹窗不需要title, info等这些信息, 也需要将对应的tag标出出来, 一共有5个
         Beta.upgradeDialogLayoutId = R.layout.layout_upgrade_dialog
-
+        //Beta.dialogFullScreen = true
         // CrashReport.initCrashReport(applicationContext, Constant.BUGLY_ID, BuildConfig.DEBUG, strategy)
         Bugly.init(applicationContext, Constant.BUGLY_ID, BuildConfig.DEBUG, strategy)
     }
@@ -125,7 +142,6 @@ class App : Application() {
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
         MultiDex.install(base)
-
         // 安装 Tinker
         Beta.installTinker()
     }
@@ -187,7 +203,7 @@ class App : Application() {
             .showThreadInfo(false)  // 隐藏线程信息 默认：显示
             .methodCount(0)         // 决定打印多少行（每一行代表一个方法）默认：2
             .methodOffset(7)        // (Optional) Hides internal method calls up to offset. Default 5
-            .tag("hao_zz")   // (Optional) Global tag for every log. Default PRETTY_LOGGER
+            .tag("wan_android")   // (Optional) Global tag for every log. Default PRETTY_LOGGER
             .build()
         Logger.addLogAdapter(object : AndroidLogAdapter(formatStrategy) {
             override fun isLoggable(priority: Int, tag: String?): Boolean {
@@ -196,7 +212,33 @@ class App : Application() {
         })
     }
 
-    private val mActivityLifecycleCallbacks = object : Application.ActivityLifecycleCallbacks {
+    private val upgradeStateListener = object : UpgradeStateListener {
+        override fun onDownloadCompleted(isManual: Boolean) {
+        }
+
+        override fun onUpgradeSuccess(isManual: Boolean) {
+        }
+
+        override fun onUpgradeFailed(isManual: Boolean) {
+            if (isManual) {
+                showToast(getString(R.string.check_version_fail))
+            }
+        }
+
+        override fun onUpgrading(isManual: Boolean) {
+            if (isManual) {
+                showToast(getString(R.string.check_version_ing))
+            }
+        }
+
+        override fun onUpgradeNoVersion(isManual: Boolean) {
+            if (isManual) {
+                showToast(getString(R.string.check_no_version))
+            }
+        }
+    }
+
+    private val mActivityLifecycleCallbacks = object : ActivityLifecycleCallbacks {
         override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
             Log.d(TAG, "onCreated: " + activity.componentName.className)
         }
