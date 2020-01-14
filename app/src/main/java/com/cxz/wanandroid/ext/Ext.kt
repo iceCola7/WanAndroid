63ddeaec60be9c7641d392e56d16b97901d1505a
chenxz <563859095@qq.com>
2020-01-14 22:37:50 +0800

    commit

--- app/src/main/java/com/cxz/wanandroid/ext/Ext.kt
+++ app/src/main/java/com/cxz/wanandroid/ext/Ext.kt
@@ -6,10 +6,12 @@ import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.util.Log
+import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Checkable
 import android.widget.TextView
 import com.cxz.wanandroid.R
 import com.cxz.wanandroid.app.App
@@ -56,6 +58,22 @@ fun Fragment.showSnackMsg(msg: String) {
     view.findViewById<TextView>(R.id.snackbar_text)
             .setTextColor(ContextCompat.getColor(this.activity!!, R.color.white))
     snackbar.show()
+}
+
+// 扩展点击事件属性(重复点击时长)
+var <T : View> T.lastClickTime: Long
+    set(value) = setTag(1766613352, value)
+    get() = getTag(1766613352) as? Long ?: 0
+
+// 重复点击事件绑定
+inline fun <T : View> T.setSingleClickListener(time: Long = 1000, crossinline block: (T) -> Unit) {
+    setOnClickListener {
+        val currentTimeMillis = System.currentTimeMillis()
+        if (currentTimeMillis - lastClickTime > time || this is Checkable) {
+            lastClickTime = currentTimeMillis
+            block(this)
+        }
+    }
 }
 
 /**
